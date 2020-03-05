0703f8c653f40b1d36d46dc2288310ceeb1754fd
EviNikolaidou <evi.nikolaidou@outlook.com>
2020-03-05 16:36:05 +0000

    check codecy

--- src/main/scala/day2/patternmatching.scala
+++ src/main/scala/day2/patternmatching.scala
@@ -3,7 +3,7 @@ package day2
 object patternmatching extends App {
  // Pattern Matching
     val  a = 5
-    val = 6
+    val b = 6
     val c = "True"
     c match {
     case "True" => println (a + b)
