c3fb6c9552164cb7c5e4f7328e374e000830665d
EviNikolaidou <evi.nikolaidou@outlook.com>
2020-03-06 14:59:54 +0000

    PaperSissorRock

--- src/main/scala/day5/papersissorrock.scala
+++ src/main/scala/day5/papersissorrock.scala
@@ -1,59 +1,79 @@
 package day5
 
-import org.graalvm.compiler.lir.sparc.SPARCMove.Move
 
 //1) def for player's move, num of players, add move to list
 //2) def AImove , countnumber of each move player and make a move
 //3) def winner find the winner
 
 import scala.util.Random
+import collection.mutable.Map
+import java.io.Console;
 
 object papersissorrock extends App {
 
   println("Paper-Scissors-Rock!")
-  //for enter pressed do this:
-  playerMove()
-  //if i>3 airandom else aicalc
+  val input = readLine("prompt> ")
+  var i = 0
+  while (true) {
+    playerMove()
+    i = i + 1
+    if (i<3) {
+     var AImove = AImoveRandom()
+    }
+    else {
+      var AImove = AImoveCalc()
+    }
+
+    winner(AImove, playerMove)
+
+  }
 
 
   def playerMove() = {
 
     //val numPlayers = scala.io.StdIn.readLine("NUMBER OF PLAYERS (1 OR 2):")
-    val movePlayer = scala.io.StdIn.readLine("Press P for paper , S for scissors and R for rock")
-    movePlayer.toUpperCase().toList
-    pretPMove(movePlayer)
+    val playerMove = scala.io.StdIn.readLine("Press P for paper , S for scissors and R for rock")
+    playerMove.toUpperCase().toList
   }
 
-  def pretPMove(movePlayer: List) = {
-    val countPaper = movePlayer.count((_ == "P"))
-    val countScissors = movePlayer.count((_ == "S"))
-    val countRock = movePlayer.count((_ == "R"))
+  def pretPMove(playerMove:List)  {
+    val countPaper = playerMove.count((_ == "P"))
+    val countScissors = playerMove.count((_ == "S"))
+    val countRock = playerMove.count((_ == "R"))
     if ((countPaper > countScissors) && (countPaper > countRock)) {
-      val predmove = "P" }
+      AImoveCalc("P")}
     if ((countScissors > countPaper) && (countScissors > countRock)) {
-      val predmove = "S" }
+      AImoveCalc("S")}
     if ((countRock > countPaper) && (countRock > countScissors)) {
-      val predmove = "R" }
+      AImoveCalc("R") }
   }
 
   def AImoveRandom() = {
-    val gesture = List("Paper", "Scissors", "Rock")
-    val AImove = Random.shuffle(gesture).head
-    AImove
+    val gesture = Random.shuffle(List("Paper", "Scissors", "Rock")).head
   }
 
- def AImoveCalc(pretMove) = {
-   pretPMove() match {
-     case "P"  => val AIMove ="S"
-     case "S"  => val AIMove ="R"
-     case "R"  => val AIMove ="P"
+ def AImoveCalc(pretMove: String) = {
+   pretMove match {
+     case "P"  => "S"
+     case "S"  => "R"
+     case "R"  => "P"
    }
  }
 
-  def winner(): = {
-    var winner = ""
-    if (AImove == movePlayer) {
-      winner = "It's a tie!"}
+  def winner() = {
+    val ruleMap : Map[(String,String),String] = Map()
+    ruleMap(("R","S")) = "R"
+    ruleMap(("S","R")) = "R"
+
+    ruleMap(("R", "P")) = "P"
+    ruleMap(("P", "R")) = "P"
+
+    ruleMap(("P","S") )="S"
+    ruleMap(("S","P") )="S"
+
+    if (AImove == playerMove) {
+       "It's a tie!"}
+    if
 
     }
 
--- src/main/scala/day5/trials.scala
+++ src/main/scala/day5/trials.scala
@@ -1,10 +1,4 @@
 package day5
 
 object trials extends App{
-  val moves = List("P", "S", "R", "P", "P", "S" )
-  val countPaper = moves.count((_ == "P"))
-  val countScissors = moves.count((_ == "S"))
-  val countRock = moves.count((_ == "R"))
-  val predMove = List(countPaper,countScissors,countRock).sorted.tail
-  println(predMove)
-}
+
